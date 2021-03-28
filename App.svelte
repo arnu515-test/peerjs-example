cb7a416e91e5480317d2f379060e8d50c94a01bf
arnu515-test <74849961+arnu515-test@users.noreply.github.com>
2021-03-28 12:58:45 +0530

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Caller from "./components/Caller.svelte";
+
+  let callerId = null;
+</script>
+
+{#if callerId === null}
+  <p class="w3-center">
+    <button on:click={() => callerId = 1} class="w3-button w3-blue w3-margin-right">1</button>
+    <button on:click={() => callerId = 2} class="w3-button w3-blue">2</button>
+  </p>
+{:else}
+  <Caller id={callerId} />
+{/if}
---
+++ components/Caller.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Peer from "peerjs";
+
+  export let id;
+  const oppId = id === 1 ? 2 : 1;
+  const peer = new Peer("arnu515s-godly-minecraft-com-" + id.toString());
+  let connected = false;
+  let oppConn = null;
+  let initiator = false;
+  let message = "";
+
+  peer.on("open", () => {
+    console.info("Peer opened");
+
+    peer.on("connection", conn => {
+      if (connected) {
+        conn.send("Already connected to a peer");
+        conn.close();
+        return;
+      }
+      conn.on("open", () => {
+        console.log("got conn");
+        connected = true;
+        oppConn = conn;
+        conn.on("data", data => console.log("recv", data));
+        conn.send("test");
+      });
+    });
+  });
+
+  function call() {
+    console.log("calling");
+    let conn = peer.connect("arnu515s-godly-minecraft-com-" + oppId.toString());
+    connected = !!conn;
+    if (!connected) return alert("Failed to connect");
+    oppConn = conn;
+    initiator = true;
+    conn.on("data", data => console.log("recv", data));
+    conn.send("test");
+  }
+
+  function send() {
+    console.log("send ");
+    if (!connected || !message) return;
+
+    oppConn.send(message.trim());
+  }
+</script>
+
+<div class="w3-container">
+  <h1>{id}</h1>
+  {#if !connected}
+    <p><button on:click={call}>Call other peer</button></p>
+  {:else}
+    <input type="text" class="w3-input w3-border" bind:value={message}>
+    <p>
+    <button class="w3-button w3-blue" on:click={send}>Send</button></p>
+  {/if}
+</div>
